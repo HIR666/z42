36919f77fa476f3d8f93faffcecbb41f2be1dd71
HIR666 <stinker.muthafuker@gmail.com>
2023-07-16 08:31:37 +0300

    adapted exactly like the devgenius guide

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@
 //import { useState } from "react";
 
 import "./App.css";
-import { Outlet } from "react-router-dom";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AboutScreen from "./screens/AboutScreen";
+import HomeScreen from "./screens/HomeScreen";
 import MainAppBar from "./components/AppBar";
 import { ThemeProvider, createTheme, CssBaseline, Box } from "@mui/material";
 import texture from "./assets/TEXTURE.jpg";
@@ -23,24 +24,28 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-
-      <MainAppBar />
-      <Box
-        component="div"
-        sx={{
-          position: "fixed",
-          width: "100%",
-          height: "100%",
-          backgroundImage: `linear-gradient(0deg, rgba(0,0,0,0.3), rgba(0,0,0,0.3)), url(${texture})`,
-          // backgroundPosition: "center",
-          backgroundSize: "cover",
-          // backgroundRepeat: "no-repeat",
-          overflowY: "scroll",
-          paddingBottom: 20,
-        }}
-      >
-        <Outlet />
-      </Box>
+      <BrowserRouter basename={import.meta.env.DEV ? "/" : "/z42/"}>
+        <MainAppBar />
+        <Box
+          component="div"
+          sx={{
+            position: "fixed",
+            width: "100%",
+            height: "100%",
+            backgroundImage: `linear-gradient(0deg, rgba(0,0,0,0.3), rgba(0,0,0,0.3)), url(${texture})`,
+            // backgroundPosition: "center",
+            backgroundSize: "cover",
+            // backgroundRepeat: "no-repeat",
+            overflowY: "scroll",
+            paddingBottom: 20,
+          }}
+        >
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/about" element={<AboutScreen />} />
+          </Routes>
+        </Box>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,31 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import AboutScreen from "./screens/AboutScreen";
-import HomeScreen from "./screens/HomeScreen";
-
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/z42/",
-    element: <App />,
-    children: [
-      {
-        path: "/z42/",
-        element: <HomeScreen />,
-      },
-      {
-        path: "/z42/about",
-        element: <AboutScreen />,
-      },
-    ],
-  },
-  { basename: import.meta.env.DEV ? "/" : "/z42/" },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
